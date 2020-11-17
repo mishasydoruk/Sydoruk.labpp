3d0523ce36f177c943bf305787aa94ba2d7aefd2
mishasydoruk <55615856+mishasydoruk@users.noreply.github.com>
2020-11-18 01:12:45 +0200

    Changed file "app"

--- Project/app.py
+++ Project/app.py
@@ -3,9 +3,9 @@ from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
-@app.route('/api/v1/hello-word-2')
+@app.route('/api/v1/hello-word-26')
 def index():
-    return "Hello world 2"
+    return "Hello world 26"
 
 if __name__=="__main__":
     WSGIServer(('', 5000), app).serve_forever()
